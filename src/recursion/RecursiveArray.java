8d8ceed3d34ad9d985ce260fcdd2a9a5b74b623b
Selena X <selenaxx20@gmail.com>
2026-03-12 20:41:59 -0700

    completed

---
+++ src/recursion/RecursiveArray.java
@@ -0,0 +1,22 @@
+package recursion;
+import java.util.Arrays;
+
+public class RecursiveArray {
+    public static void main(String[] args) {
+    
+        int[] array = {1,2,3,4,5};
+        doubleArray(array);
+        System.out.print(Arrays.toString(array));
+    }
+
+    // double the elements in an array
+    public static void doubleArray(int[] array) {
+        doubleArrayHelper(array, 0);
+    }
+    private static void doubleArrayHelper(int[] array, int index) {
+        if(index < array.length) {
+        array[index] *= array[index];
+        doubleArrayHelper(array, index + 1);
+        }
+    }
+}
