3535d746aba38f2e90d558d74a711dbdd5e7c27c
Selena X <selenaxx20@gmail.com>
2026-03-29 12:26:24 -0700

    Implement index + substring recursion

--- src/recursion/CharFrequency.java
+++ src/recursion/CharFrequency.java
@@ -2,20 +2,30 @@ package recursion;
 
 /*
  Problem: return # of times char appears in string
+
  Approach:
  1. check current index match target
  2. check next index
  3. base case: when index reaches the end of string
 
- Time: O(n); 1 recursive call per n
- Space: O(n); recursion stack depth n
+ 2 Methods:
+    - Index recursion:
+         Time: O(n); 1 recursive call per n
+         Space: O(n); recursion stack depth n
+    - Substring recursion:
+         Time: O(n^2); 1 recursive call + make copy of string per n
+         Space: O(n^2); depth n + new string each call
+
  */
 public class CharFrequency {
+
+    // Method 1: helper method
+        // Most efficient - updates index pointer each call vs each call creates a new substring
     public static int charFrequency(String word, char target) {
-        return charFrequency(word, target, 0);
+        return charFrequencyHelper(word, target, 0);
     }
 
-    public static int charFrequency(String word, char target, int index) {
+    public static int charFrequencyHelper(String word, char target, int index) {
         int count = 0;
         if (index == word.length()) {
             return count;
@@ -24,6 +34,25 @@ public class CharFrequency {
         if (word.charAt(index) == target) {
             count++;
         }
-        return count + charFrequency(word, target, index + 1);
+        return count + charFrequencyHelper(word, target, index + 1);
+    }
+
+    // Method 2: update local variable with recursive call and return variable
+    public static int charFrequencyMethod2(String word, char target) {
+
+        if(word.isEmpty()) return 0;
+
+        int count = charFrequencyMethod2(word.substring(1), target);
+        if(word.charAt(0) == target) count++;
+        return count;
+    }
+
+    // Method 3: return recursive call
+    public static int charFrequencyMethod3(String word, char target) {
+
+        int count = 0;
+        if(word.isEmpty()) return 0;
+        if(word.charAt(0) == target) count++;
+        return count + charFrequencyMethod3(word.substring(1), target);
     }
 }
