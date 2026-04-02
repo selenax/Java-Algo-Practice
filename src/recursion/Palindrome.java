b8b5d4a73efd645903e768a3229819bdf0da8700
Selena X <selenaxx20@gmail.com>
2026-04-02 13:03:57 -0700

    implement recursion

---
+++ src/recursion/Palindrome.java
@@ -0,0 +1,25 @@
+package recursion;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        System.out.println(palindrome("ac"));
+        System.out.println(palindrome("aba"));
+        System.out.println(palindrome("abab"));
+        System.out.println(palindrome("ababa"));
+        System.out.println(palindrome(""));
+    }
+    public static boolean palindrome(String word) {
+          return palindromeHelper(0, word.length()-1, word);
+    }
+
+    private static boolean palindromeHelper(int start, int end, String word) {
+        int mid = word.length()/2;
+        if(start >= mid) {
+            return true;
+        }
+        if(word.charAt(start) != word.charAt(end)) {
+            return false;
+        }
+        return palindromeHelper(start + 1, end - 1, word);
+    }
+}
